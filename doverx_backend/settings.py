7f77c9626b2910c9c3aa6d0a017a363c5d28e369
vietTNT <trandacdaiviet@gmail.com>
2025-12-02 15:58:51 +0700

    fix OTP gmail

--- doverx_backend/settings.py
+++ doverx_backend/settings.py
@@ -227,8 +227,11 @@ REST_FRAMEWORK = {
 # =========================================================
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True  
+EMAIL_USE_TLS = False
 EMAIL_HOST_USER = "trandacdaiviet@gmail.com"
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
